4669ddafd6ed7f38b262b23b996ba7d69db7024a
HaecheonLee <dlgocjs1@gmail.com>
2019-12-06 14:57:23 -0500

    boj.kr/10989
    sorting

---
+++ PS/BAEKJOON/수 정렬하기 3.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+#define MAX 10001
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+
+    int N;
+    cin>>N;
+
+    int count[MAX] { 0 };
+    int num;
+    for(int i=0; i<N; i++) {
+        cin>>num;
+        count[num]++;
+    }
+
+    /* Counting Sort */
+    int counted = 0;
+    for(int i=1; i<MAX; i++) {
+        for(int j=0; j<count[i]; j++) {
+            cout<<i<<"\n";
+            counted++;
+        }
+
+        if(counted == N) break;
+    }
+
+    return 0;
+}
