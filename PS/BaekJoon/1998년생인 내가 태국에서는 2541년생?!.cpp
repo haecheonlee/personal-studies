8d362ed85425f26b1bcc21f848980182393828c8
HaecheonLee <dlgocjs1@gmail.com>
2019-12-21 18:41:14 -0500

    Create 1998년생인 내가 태국에서는 2541년생?!.cpp

---
+++ PS/BaekJoon/1998년생인 내가 태국에서는 2541년생?!.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int y=0;
+	cin>>y;
+	
+	int result = y-543;
+	cout<<result;
+	
+	return 0;
+}
+
+/*
+boj.kr/18108
+
+INPUT:
+2541
+
+OUTPUT:
+1998
+*/
