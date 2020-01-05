5f6df1a41f877314d6cf46a90f7bf13d1b74704b
HaecheonLee <dlgocjs1@gmail.com>
2020-01-04 23:34:18 -0500

    Update 피자는 나눌 수록 커지잖아요.cpp

--- PS/BaekJoon/피자는 나눌 수록 커지잖아요.cpp
+++ PS/BaekJoon/피자는 나눌 수록 커지잖아요.cpp
@@ -1,23 +1,19 @@
 #include <iostream>
-#include <cmath>
 
 using namespace std;
 
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(nullptr);
-	
 	int T;
 	cin>>T;
 	
-	long long c;
+	unsigned long long c;
 	while(T--) {
 		cin>>c;
 		
-		//long long count=((c+1)*c)/2;
-		//long long total_pizza=(pow(c,2)+c+2)/2;
+		unsigned long long count=((c+1)*c)/2;
+		unsigned long long total_pizza=((c*c)+c+2)/2;
 		
-		cout<<(1)<<'\n';
+		cout<<total_pizza-count<<'\n';
 	}
 
 	return 0;
