ef37f2a3e61d1b97164b2ed75a2a555337deba3b
HaecheonLee <dlgocjs1@gmail.com>
2020-01-04 20:14:22 -0500

    Create 피자는 나눌 수록 커지잖아요.cpp

---
+++ PS/BaekJoon/피자는 나눌 수록 커지잖아요.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	
+	int T;
+	cin>>T;
+	
+	long long c;
+	while(T--) {
+		cin>>c;
+		
+		//long long count=((c+1)*c)/2;
+		//long long total_pizza=(pow(c,2)+c+2)/2;
+		
+		cout<<(1)<<'\n';
+	}
+
+	return 0;
+}
+
+/*
+boj.kr/17946
+
+INPUT:
+2
+1
+2
+
+OUTPUT:
+1
+1
+*/
