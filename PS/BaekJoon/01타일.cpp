6b0208f601d38ee2e0e80f130cbbc84f2498b48a
HaecheonLee <dlgocjs1@gmail.com>
2020-01-07 10:45:32 -0500

    Create 01타일.cpp

---
+++ PS/BaekJoon/01타일.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+#define mod 15746
+
+int DP[1000001]{0};
+int main() {
+	int N;
+	cin>>N;
+	
+	DP[0]=1; DP[1]=1;
+	for(int i=2; i<=N; i++) {
+		DP[i]=(DP[i-1]%mod)+(DP[i-2]%mod);	
+	}
+	
+	cout<<DP[N]%mod;
+	return 0;
+}
+
+/*
+boj.kr/1904
+
+INPUT:
+4
+
+OUTPUT:
+5
+*/
