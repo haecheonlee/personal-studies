d60077cc5bd91087375865bb5562da55fcedf986
HaecheonLee <dlgocjs1@gmail.com>
2020-03-07 19:25:24 -0500

    Create 돌 게임.cpp

---
+++ PS/BaekJoon/돌 게임.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int n;
+	cin>>n;
+	
+	if((n%2)==0) cout<<"CY";
+	else cout<<"SK";
+
+	return 0;
+}
+
+/*
+boj.kr/9655
+
+INPUT:
+5
+
+OUTPUT:
+SK
+*/
