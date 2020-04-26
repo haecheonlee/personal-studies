d035e8eebfcac29160e28c191d0dbf68f14a4f72
HaecheonLee <dlgocjs1@gmail.com>
2020-04-26 12:03:57 -0400

    boj.kr/5522

---
+++ PS/BaekJoon/카드 게임.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int sum=0;
+	
+	int num;
+	while(cin>>num) sum+=num;
+	cout<<sum;
+
+	return 0;
+}
+
+/*
+INPUT:
+1
+2
+3
+4
+5
+
+OUTPUT:
+15
+*/
