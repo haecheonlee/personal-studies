8a3dea7def1e0d6e8be4406805a4db62bb8294f9
HaecheonLee <dlgocjs1@gmail.com>
2020-06-29 21:26:58 -0400

    # 1374B

---
+++ PS/Codeforces/B. Multiply by 2, divide by 6.cpp
@@ -0,0 +1,38 @@
+/* Codeforces */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int T;
+    cin>>T;
+    
+    while(T--) {
+        int n;
+        cin>>n;
+        
+        int two=0, three=0;
+        while(n%2==0) {
+            n/=2;
+            two++;
+        }
+        
+        while(n%3==0) {
+            n/=3;
+            three++;
+        }
+    
+        if(n!=1 || two>three) cout<<"-1\n";
+        else if(three==0 && two==0) cout<<"0\n";
+        else {
+            if(two==0) cout<<three*2<<'\n';
+            else cout<<three+(three-two)<<'\n';
+        }
+    }
+    
+	return 0;
+}
