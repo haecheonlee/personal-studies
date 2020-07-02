d788dd232b5e37e8d962cb152dfb4e245984501c
HaecheonLee <dlgocjs1@gmail.com>
2020-07-02 17:33:48 -0400

    # 1337A

---
+++ PS/Codeforces/635/A. Ichihime and Triangle.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+ 
+using namespace std;    
+ 
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int t;
+    cin>>t;
+    
+    while(t--) {
+        int a,b,c,d;
+        cin>>a>>b>>c>>d;
+        
+        cout<<b<<" "<<c<<" "<<c<<'\n';
+    }
+    
+    return 0;
+}
