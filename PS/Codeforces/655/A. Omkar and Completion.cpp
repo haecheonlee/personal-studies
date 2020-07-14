1cbd761ec9c77c24b5391d07dd9137986021e7fd
HaecheonLee <dlgocjs1@gmail.com>
2020-07-14 14:13:22 -0400

    # 1372A

---
+++ PS/Codeforces/655/A. Omkar and Completion.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int T;
+	cin>>T;
+	
+	while(T--) {
+		int N;
+		cin>>N;
+			
+		for(int i=0; i<N; i++) cout<<1<<" ";
+		cout<<'\n';
+	}
+
+	return 0;
+}
