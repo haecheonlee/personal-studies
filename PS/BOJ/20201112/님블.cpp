68637d983704942a7d4b43029acc841f3f9eae38
HaecheonLee <dlgocjs1@gmail.com>
2020-11-12 13:29:38 -0500

    boj.kr/11869

---
+++ PS/BOJ/20201112/님블.cpp
@@ -0,0 +1,22 @@
+/* reference: baekjoon/codeplus */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int N;
+	cin >> N;
+	
+	int ans = 0;
+	for(int i = 0; i < N; i++) {
+		int x;
+		cin >> x;
+		
+		ans ^= x;
+	}
+	
+	cout << (ans ? "koosaga" : "cubelover");
+
+	return 0;
+}
