1542139b525df136ad9833e4c5e95c9381e82e69
HaecheonLee <dlgocjs1@gmail.com>
2020-12-02 10:38:24 -0500

    #1455A

---
+++ PS/Codeforces/UpSolving/20201202/A. Strange Functions.cpp
@@ -0,0 +1,19 @@
+/* reference: editorial */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int T;
+	cin >> T;
+	
+	while(T--) {
+		string s;
+		cin >> s;
+		
+		cout << s.size() << '\n';
+	}
+	
+	return 0;
+}
