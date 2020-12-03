baf2878da23501ebf97ad95eb6fcc192ee8eaa3d
HaecheonLee <dlgocjs1@gmail.com>
2020-12-03 13:24:00 -0500

    #1455C

---
+++ PS/Codeforces/UpSolving/20201203/C. Ping-pong.cpp
@@ -0,0 +1,19 @@
+/* editorial */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+    
+    while(T--) {
+        int x, y;
+        cin >> x >> y;
+        
+        cout << x - 1 << ' ' << y << '\n';
+    }
+    
+    return 0;
+}
