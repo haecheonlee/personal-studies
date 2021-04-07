819da174e106c54705c63ec420c50ff9418d1c45
HaecheonLee <dlgocjs1@gmail.com>
2021-04-07 01:50:54 -0400

    boj.kr/2900
    optimized

--- PS/BOJ/20210407/PROGRAM.cpp
+++ PS/BOJ/20210407/PROGRAM.cpp
@@ -1,53 +1,27 @@
-/* BOJ */
-
-/* reference: codeplus/beakjoon */
-/* ---------------------------------- template starts ---------------------------------- */
 #include <iostream>
-#include <vector>
-#include <queue>
-#include <set>
-#include <map>
-#include <algorithm>
-#include <cstring>
 
 #define fastio ios_base::sync_with_stdio(0); cin.tie(0);
-#define fi first
-#define sc second
-#define sz(x) (int)(x.size())
-#define all(x) (x).begin(), (x).end()
-#define eb emplace_back
 
 using namespace std;
 
 typedef long long ll;
-typedef pair<int,int> pii;
-typedef pair<ll,int> pli;
-typedef pair<ll,ll> pll;
-
-int dx[] = {-1,1,0,0};
-int dy[] = {0,0,-1,1};
-/* ---------------------------------- template ends ---------------------------------- */
 
 const int MAX_N = 1e6 + 10;
 
 int N, K, Q;
-ll a[MAX_N], cnt[MAX_N], p[MAX_N];
-set<int> list;
+ll cnt[MAX_N], p[MAX_N];
 
 int main() {
-    fastio
-	cin >> N >> K;
+	fastio
+   	cin >> N >> K;
 	for(int i = 1; i <= K; i++) {
-		cin >> a[i];
-		cnt[a[i]]++;
-        list.insert(a[i]);
+        	int x; cin >> x;
+       		cnt[x]++;
 	}
 
 	p[0] = K;
-    for(auto& it : list) {
-            for(int k = it; k < N; k += it) p[k] += cnt[it];
-    }
-    for(int i = 1; i < N; i++) p[i] += p[i - 1];
+    	for(int i = 0; i < N; i++) if(cnt[i]) for(int j = i; j < N; j += i) p[j] += cnt[i];
+    	for(int i = 1; i < N; i++) p[i] += p[i - 1];
 	cin >> Q;
 	
 	while(Q--) {
