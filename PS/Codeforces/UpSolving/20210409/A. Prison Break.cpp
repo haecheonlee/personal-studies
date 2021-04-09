2f83916f47e4432aeb113dec26d7887b8cfd3807
HaecheonLee <dlgocjs1@gmail.com>
2021-04-09 15:01:24 -0400

    #1484A

---
+++ PS/Codeforces/UpSolving/20210409/A. Prison Break.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+void solve() {
+	int a, b; cin >> a >> b;
+	cout << a * b << '\n';
+}
+
+int main() {
+	int T; cin >> T;
+	while(T--) solve();
+	
+	return 0;
+}
