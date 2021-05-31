4cdfffd965c7e7f94b3416e15b98b6f64d978fba
HaecheonLee <dlgocjs1@gmail.com>
2021-05-30 20:06:45 -0400

    boj.kr/11378
    bipartite-matching

---
+++ PS/BOJ/20210530/열혈강호 4.cpp
@@ -0,0 +1,78 @@
+/* reference: codeplus/beakjoon */
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct MaximumFlow {
+	int N, K;
+	vector<vector<int>> G;
+	vector<bool> check;
+	vector<int> pred;
+	
+	MaximumFlow(int N, int K) : N(N), K(K) {
+		G.resize(N);
+		check.resize(N);
+		pred.resize(N, -1);
+	}
+	
+	void add_edge(int u, int v) {
+		G[u].push_back(v);
+	}
+	
+	bool dfs(int x) {
+		if(x == -1) return true;
+		for(auto& y : G[x]) {
+			if(check[y]) continue;
+			check[y] = 1;
+			
+			if(dfs(pred[y])) {
+				pred[y] = x;
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	int flow() {
+		int ans = 0;
+		for(int i = 0; i < N; i++) {
+			fill(check.begin(), check.end(), false);
+			if(dfs(i)) ans++;
+		}
+		
+		int k = 0;
+		for(int i = 0; i < N; i++) {
+			while(1) {
+				fill(check.begin(), check.end(), false);
+				if(dfs(i)) {
+					ans++;
+					k++;
+				} else {
+					break;
+				}
+				if(k == K) break;
+			}
+			if(k == K) break;
+		}
+		
+		return ans;
+	}
+};
+
+int main() {
+	int N, M, K; cin >> N >> M >> K;
+	
+	MaximumFlow mf(max(N, M), K);
+	for(int i = 0; i < N; i++) {
+		int q; cin >> q;
+		while(q--) {
+			int x; cin >> x;
+			mf.add_edge(i, x - 1);
+		}
+	}
+	cout << mf.flow() << '\n';
+	
+	return 0;
+}
